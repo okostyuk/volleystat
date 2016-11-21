bcaf0d14b2642adda0f3a6103da0d2eee393aa5f
oleg <oleg.kostyuk@workrocks.com>
2016-11-22 00:46:26 +0200

    game screen

--- app/src/main/java/ua/org/volley/stat/activity/GameActivity.java
+++ app/src/main/java/ua/org/volley/stat/activity/GameActivity.java
@@ -1,7 +1,10 @@
 package ua.org.volley.stat.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import ua.org.volley.stat.R;
 
@@ -11,5 +14,21 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.game_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_view_log:
+                //startActivity(new Intent(this, GameActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/ua/org/volley/stat/activity/SelectTeamsActivity.java
+++ app/src/main/java/ua/org/volley/stat/activity/SelectTeamsActivity.java
@@ -1,10 +1,13 @@
 package ua.org.volley.stat.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
@@ -28,6 +31,7 @@ public class SelectTeamsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_teams);
 
+
         RecyclerView list = (RecyclerView) findViewById(R.id.teamsList);
         list.setLayoutManager(new LinearLayoutManager(this));
 
@@ -46,5 +50,21 @@ public class SelectTeamsActivity extends AppCompatActivity {
 
         TeamSelectAdapter adapter = new TeamSelectAdapter(teams.orderByValue(), Team.class);
         list.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.select_tesams_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_teams_selected:
+                startActivity(new Intent(this, GameActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
